6fe368c3c1a07a29c909df039b923e577a8d4f45
Soko77788 <NickSokolowski7@gmail.com>
2024-10-21 09:37:00 -0500

    final bugs squashed

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     duration: 10
   }); //
 
-  function handleChange(newValue, inputIdentifier) {
+  function handleChange(inputIdentifier, newValue, ) {
     setUserInput(prevUserInput => {
         return {
             ...prevUserInput,
@@ -21,11 +21,13 @@ function App() {
     });
 }
 
+const inputIsValid = userInput.duration >=1;
   return (
     <>
    <Header />
    <UserInput userInput={userInput} onChange={handleChange} />
-   <Results input={userInput} />
+   {!inputIsValid && ( <p className="center">Please enter a duration greater than zero.</p>)}
+   {inputIsValid && <Results input={userInput} />}
    </>
   )
 }
